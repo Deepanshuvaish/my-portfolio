92352393bdf80ce8bb434ced190c51546d07685f
Deepanshuvaish <deepanshuvaish26@gmail.com>
2023-11-08 12:49:39 +0530

    initial commit for portfolio

---
+++ figma template/second.js
@@ -0,0 +1,167 @@
+// var age = prompt("enter your age")
+
+
+// if (age>=18){
+//     alert("adult");
+// }
+// else if (age>=13 &&  age <18){
+//     alert("teenager");
+// } else{
+//     alert("child")
+// }
+
+
+// age >=18 ? console.log("adult")
+// :age>=13 && age <18 
+// ? console.log("teenager")
+// :console.log("child")
+
+// age >= 6 && age < 13 
+// ? console.log("child"):
+// console.log("toddler")
+
+
+
+
+// var proffession = prompt("enter yoiur profession")
+// var profession = proffession.toLowerCase();
+// switch(proffession){
+//     case 'doctor':
+//         console.log('work at hospital');
+// break;
+
+// case 'cop':
+// console.log('work at police station');
+// break;
+
+// case 'teacher':
+//     console.log("work at school")
+// break;
+// }
+
+
+// var week = ("enter week days")
+
+// switch('week'){
+// case '1':
+// console.log("sunday")
+// case '2':
+// console.log("monday")
+// case '3':
+//     console.log("tuesday")
+// case '4':
+//     console.log("wednesday")
+// case '5':
+// console.log("thurday")
+// case'6':
+// console.log("friday")
+// case '7':
+//     console.log("saturday")
+// }
+
+
+
+
+// var days = prompt("days in a months")
+
+// switch(days){
+//     case "january":
+//     console.log('31 days')
+//     break;
+
+// case 'february' :
+// if((year % 4 === 0) && (year % 100)||(0=== year %400)){
+    
+// }
+// console.log('28 days')
+// break;
+
+
+
+// case 'march' :
+//     console.log('31 days')
+//     break;
+
+// case 'april':
+// console.log('31 days')
+// break;
+
+// case 'may':
+// console.log('31 days')
+// break;
+
+// case 'june':
+// console.log('30 days')
+// break;
+
+// case 'july':
+// console.log('31 days')
+// break;
+
+// case 'august':
+// console.log('31 days')
+// break;
+
+// case 'september':
+// console.log('30 days')
+// break; 
+
+// case 'october':
+// console.log('31 days')
+// break;
+
+// case 'november':
+// console.log('30 days')
+// break;
+
+// case 'december':
+// console.log('31 days')
+// break;
+// }
+
+
+// let year= parseFloat(prompt("enter the year"))
+// let month = parseFloat(prompt("enter the month"))
+// monthname
+// switch(month){
+//     case "1":
+//     case '3':
+//     case '5':
+//     case '6':
+//     case '7':
+//     case '8':
+//     case '10':
+//     case '12':
+    
+//     if(1){
+//         monthname="january"
+//     }
+
+//         alert("no of days are 31")
+// break;
+
+// case '4':
+// case '6':
+// case '9':
+// case '11':
+// alert("no of days are 30")
+// break;
+
+// case '2':
+//     if((year % 4 ===0) && (year % 100 !=0)||(year % 400===0))
+// {
+//     ("no of days are 28 _ _ _ _ leap year")
+// }
+// {
+//     alert("no of days are 29 _ _ _ _Not a leap year")
+// }
+
+// 
+var age = prompt('enter your age')
+
+if(age =>18){
+    console.log("adul"t)
+}
+else if(age=>13 && age<18){
+alert("teenager")
+}
